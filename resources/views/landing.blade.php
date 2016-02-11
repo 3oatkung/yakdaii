006d76b3b18c627b37953c682bc4f919affec418
3oatkung <masterphaisit@hotmail.com>
2016-02-12 00:30:15 +0700

    Upload

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,271 @@
+<!doctype html>
+<html lang="en">
+   <head>
+   	
+         <meta charset="utf-8" />
+  		
+         <title>อยากได้ - เราจะตอบสนอง "ทุกความอยาก" ของคุณ</title>						
+         <meta name="description" content="" />
+    	
+         <!-- Meta Viewport -->
+         <meta name="viewport" content="width=device-width, initial-scale=1">
+		
+         <!-- All CSS Styles -->
+         <link type="text/css" href="{{url('/')}}/assets/css/bootstrap.css" rel="stylesheet" media="screen" />
+         <link type="text/css" href="{{url('/')}}/assets/css/animate.css" rel="stylesheet" media="screen" />
+         <link type="text/css" href="{{url('/')}}/assets/css/swiper.css" rel="stylesheet" media="screen" />
+         <link type="text/css" href="{{url('/')}}/assets/css/owl.carousel.css" rel="stylesheet" media="screen" />
+         <link type="text/css" href="{{url('/')}}/assets/css/owl.theme.css" rel="stylesheet" media="screen" />
+         <link type="text/css" href="{{url('/')}}/assets/css/magnific-popup.css" rel="stylesheet" media="screen" />
+         <link type="text/css" href="{{url('/')}}/assets/css/styles/style-orange.css" rel="stylesheet" media="screen" />  	
+         <link type="text/css" href="{{url('/')}}/assets/css/font-awesome.min.css" rel="stylesheet" media="screen" />
+		
+         <!-- Media Queries -->
+         <link type="text/css" href="{{url('/')}}/assets/css/media.css" rel="stylesheet" media="screen" />
+            
+         <!-- Modernizr -->
+         <script type="text/javascript" src="{{url('/')}}/assets/js/modernizr.js"></script> 	
+		
+         <!-- Comments for IE8 and Lower -->				
+		
+         <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+         <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+            <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+         <![endif]-->     
+         
+         <!-- Gradient Support IE9 -->
+         <!--[if gte IE 9]>
+            <style type="text/css">
+              .gradient {
+                 filter: none;
+              }
+            </style>
+         <![endif]-->	
+      
+	</head>
+	<body class="particles">
+  
+	      <!-- Preloader -->
+         <div id="loader-wrapper">
+             <div id="loader"></div>
+         </div>
+         <!-- Preloader End -->
+			
+         <!-- Header -->
+         <header>
+         
+               <!-- Intro -->
+               <div id="intro" class="overlay-gradient" style="height: 480px;" data-scroll-index="0">        
+
+                        <!-- Top Bar -->
+                        <div class="top-bar">
+               
+                           <div class="container">
+                              <div class="row">
+                              
+                                 <div class="col-md-12">
+                                 
+                                       <!-- Logo -->
+                                       <div class="logo"><a title="" href="#">Yakdai</a></div>
+                                       <!-- Logo End -->
+                     
+                                       <!-- Menu -->
+                                       <div class="nav">
+                     
+                                          <nav class="navbar navbar-default" role="navigation">
+                                          
+                                             <!-- Mobile Button -->
+		                                       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                                                <span class="sr-only">Toggle navigation</span>
+                                                <span class="icon-bar"></span>
+                                                <span class="icon-bar"></span>
+                                                <span class="icon-bar"></span>
+                                             </button>
+                                             <!-- Mobile Button End -->
+  
+                                          </nav>
+                        
+                                       </div>
+                                       <!-- Menu End -->
+                                       
+                                 </div>
+               
+                              </div><!-- .row End -->                  
+                           </div><!-- .container End -->
+                           
+                                             
+                        </div>
+                        <!-- Top Bar End -->
+                        
+                        <div class="container" >
+                           <div class="row">
+                              
+                                 
+                                 <div class="col-md-12">
+                                    
+                                       <!-- Intro Heading -->
+                                       <div class="headline">
+                                          <h1 class="wow fadeInDown" style="color:white;" >ที่ "อยากได้"</h1>
+                                          <h3 class="wow fadeInDown" style="color:white;" data-wow-delay="0.5s">เราสร้างระบบ เพื่อ ผู้จ้างงาน และ ฟรีแลนซ์ เพราะเราเชื่อว่าทุกคนควรจะได้รับ <u>สิ่งที่ดีที่สุด</u> </h3>
+                                          <p class="wow fadeInDown" data-wow-delay="0.5s" style="color:white;" >ไม่ว่าจะเป็น ผู้จ้างงาน <u>"อยากได้"</u> ฟรีแลนซ์ <br>หรือ ฟรีแลนซ์ <u>"อยากได้"</u> ผู้จ้างงาน <br><br>เราจะตอบสนอง <u>"ความอยาก"</u> เหล่านี้ให้กับคุณ</p>
+                                       </div>
+                                       <!-- Intro Heading End -->
+                                 </div>
+                           
+                           
+                           </div><!-- .row End -->
+
+                        </div><!-- .container End -->
+               
+               </div>
+               <!-- Intro End -->
+         
+         </header>
+         <!-- Header -->
+         
+         
+         <!-- Content -->
+         <div id="content">
+            
+               <!-- Get Started Section -->
+               <section id="get-started" class="clearfix" data-scroll-index="1">
+                  
+                     <!-- Get Started -->
+                     <div class="get-started" style="padding-bottom: 20px; padding-top: 50px;">
+                     
+                           <div class="container">
+                              <div class="row">
+                                 
+                                       <!-- Carousel -->
+                                       <div class="col-md-12">
+                                    
+                                             <div id="carousel" class="owl-carousel">
+                                          
+                                                   <!-- Item -->
+                                                   <div>
+                                                      <div class="icon-wrap wow fadeIn" data-wow-offset="300">
+                                                         <i class="fa fa-street-view icon"></i>
+                                                         <h3>ผู้จ้างงาน</h3>
+                                                         <p>เราสร้างพื้นที่สำหรับติดประกาศงาน ให้กับคุณ เพื่อให้ฟรีแลนซ์ในระบบเรา เสนอผลงานและราคา ด้วยวิธีนี้ คุณจะได้ฟรีแลนซ์ที่เหมาะสม บนราคาที่คุณพอใจ</p>
+                                                      </div>
+                                                   </div>
+                                             
+                                                   <!-- Item -->
+                                                   <div>
+                                                      <div class="icon-wrap wow fadeIn"  data-wow-offset="300">
+                                                         <i class="fa fa-laptop icon"></i>
+                                                         <h3>"อยากได้"</h3>
+                                                         <p>ระบบของเรา จะทำให้ "ผู้จ้างงาน" และ "ฟรีแลนซ์" หากันจนเจอ ยิ่งไปกว่านั้นเราจะทำให้คุณมั่นใจได้ว่า "ผู้จ้างงาน" ได้งานที่ตัวเองต้องการ และ "ฟรีแลนซ์" ได้ค่าจ้างที่เหมาะสม</p>
+                                                      </div>
+                                                   </div>
+                                                                                                      <!-- Item -->
+                                                   <div>
+                                                      <div class="icon-wrap wow fadeIn" data-wow-offset="300">
+                                                         <i class="fa fa-child icon"></i>
+                                                         <h3>ฟรีแลนซ์</h3>
+                                                         <p>คุณไม่จำเป็นต้องวิ่งหางานอีกต่อไป งานต่างๆที่ผู้จ้างงานต้องการ จะถูกส่งตรงถึงหน้าจอคุณ คุณจะมีอิสระในการเลือกงานที่ตัวเองชอบ เสนอราคาที่ใช่ และ มีอิสระกับเวลาของตัวเอง</p>
+                                                      </div>
+                                                   </div>
+                                             
+                                                   
+                                       
+                                             </div>
+                                 
+                                       </div>
+                                       <!-- Carousel End -->
+                        
+                           
+                              </div><!-- .row End -->
+                        </div><!-- .container End -->
+                        
+                     </div>
+                     <!-- Get Started End -->
+                     
+               </section>
+               <!-- Get Started Section End -->
+               
+               <!-- Newsletter Section -->
+               <section id="newsletter" class="clearfix">
+               
+                     <!-- Newsletter -->
+                     <div class="newsletter overlay-gradient">
+                        
+                           <div class="container">
+                              <div class="row">                                                                                                                                  
+                                    
+                                       <div class="col-md-8 col-md-offset-2">
+                                          
+                                             <!-- Heading -->
+                                             <div class="heading center inverted">
+                                                <h2>รับข่าวสาร จากทางเรา</h2>
+                                                <p class="subheadline">เพื่อให้กำลังใจกับเรา คุณสามารถแสดงความสนใจได้ โดยการกรอกอีเมลล์ในช่องนี้ เมื่อระบบของเราพร้อมแล้ว คุณจะเป็นคนแรกๆที่ได้สัมผัสกับโลกยุคใหม่แห่งการจ้างงาน</p>
+                                             </div>
+                                             <!-- Heading End -->
+                                             
+                                             <!-- Subscribe Form -->
+                                             <form id="newsletter-form" class="clearfix">
+                                                <input type="email" name="mc-email" id="mc-email" class="input-field form-control" placeholder="อีเมลล์ของคุณ">
+                                                <label class="subscribe-message"></label>
+                                                <input type="submit" value="สนับสนุนเรา" name="subscribe-submit" class="submit">
+                                             </form>                                            
+                                             <!-- Subscribe Form End -->
+                                       
+                                       </div>
+                              
+                              </div><!-- .row End -->
+                           </div><!-- .container End -->
+                     
+                     </div>
+                     <!-- Newsletter End -->
+               
+               </section>
+               <!-- Newsletter Section End -->
+               
+               
+
+         
+         </div>
+         <!-- Content End -->
+         
+         
+         <!-- Footer -->
+         <footer>
+
+               <div class="container">
+                  <div class="row">
+                     
+                           <div class="col-md-12">
+                              
+                                 <!-- ScrollToTop -->         
+                                 <a class="scrollup" href="#" title=""><i class="fa fa-angle-up"></i></a>         
+                                 <!-- ScrollToTop End -->
+                                 
+                                 <p>All Rights Reserved - Copyright &copy; 2016</p>
+                           
+                           </div>
+                  
+                  </div><!-- .row End -->
+               </div><!-- .container End -->
+         
+         </footer>
+         <!-- Footer -->
+
+	   
+         <!-- Javascript Files -->
+         <script type="text/javascript" src="{{url('/')}}/assets/js/jquery-1.11.2.min.js"></script> 		
+         <script type="text/javascript" src="{{url('/')}}/assets/js/bootstrap.min.js"></script>	
+         <script type="text/javascript" src="{{url('/')}}/assets/js/jquery.easing.1.3.js"></script>
+         <script type="text/javascript" src="{{url('/')}}/assets/js/scrollIt.js"></script>         
+         <script type="text/javascript" src="{{url('/')}}/assets/js/swiper.min.js"></script>
+         <script type="text/javascript" src="{{url('/')}}/assets/js/owl.carousel.min.js"></script>
+         <script type="text/javascript" src="{{url('/')}}/assets/js/jquery.magnific-popup.min.js"></script>
+         <script type="text/javascript" src="{{url('/')}}/assets/js/wow.min.js"></script>
+         <script type="text/javascript" src="{{url('/')}}/assets/js/jquery.stellar.min.js"></script>
+         <script type="text/javascript" src="{{url('/')}}/assets/js/jquery.ajaxchimp.min.js"></script>
+         <script type="text/javascript" src="{{url('/')}}/assets/js/jquery.particleground.min.js"></script>
+         <script type="text/javascript" src="{{url('/')}}/assets/js/custom.js"></script>
+					
+	</body>
+</html>
